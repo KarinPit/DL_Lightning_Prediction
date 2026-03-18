825d0639091fb079b2594611c8c3784cd0592d5d
KarinPit <karinpitlik@gmail.com>
2026-03-18 14:58:03 +0200

    Added the general code of the UNet

--- UNet_Lightning_Prediction.py
+++ UNet_Lightning_Prediction.py
@@ -76,6 +76,104 @@ def load_nc_layer(file_path, variable_name):
     return tensor
 
 
+######### Deep learning Classes #########
+
+
+class DoubleConv(nn.Module):
+    """(convolution => Batch normalization => ReLU) * 2"""
+
+    def __init__(self, in_channels, out_channels):
+        super().__init__()
+        self.double_conv = nn.Sequential(
+            nn.Conv2d(in_channels, out_channels, kernel_size=3, padding=1),
+            nn.BatchNorm2d(out_channels),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(out_channels, out_channels, kernel_size=3, padding=1),
+            nn.BatchNorm2d(out_channels),
+            nn.ReLU(inplace=True),
+        )
+
+    def forward(self, x):
+        return self.double_conv(x)
+
+
+class Down(nn.Module):
+    """Downscaling with maxpool then double conv"""
+
+    def __init__(self, in_channels, out_channels):
+        super().__init__()
+        self.maxpool_conv = nn.Sequential(
+            nn.MaxPool2d(2), DoubleConv(in_channels, out_channels)
+        )
+
+    def forward(self, x):
+        return self.maxpool_conv(x)
+
+
+class Up(nn.Module):
+    """Upscaling then double conv"""
+
+    def __init__(self, in_channels, out_channels):
+        super().__init__()
+        self.up = nn.ConvTranspose2d(
+            in_channels, in_channels // 2, kernel_size=2, stride=2
+        )
+        self.conv = DoubleConv(in_channels, out_channels)
+
+    def forward(self, x1, x2):
+        x1 = self.up(x1)
+
+        diffY = x2.size()[2] - x1.size()[2]
+        diffX = x2.size()[3] - x1.size()[3]
+
+        # if a pixel is missing (creating a size mismatch) - fix by padding
+        x1 = F.pad(x1, [diffX // 2, diffX - diffX // 2, diffY // 2, diffY - diffY // 2])
+        x = torch.cat([x2, x1], dim=1)
+        return self.conv(x)
+
+
+class UNet(nn.Module):
+    def __init__(self, n_channels=1, n_classes=1):
+        super(UNet, self).__init__()
+        self.n_channels = n_channels
+        self.n_classes = n_classes
+
+        # Encoder (Down sampling)
+        self.inc = DoubleConv(n_channels, 64)
+        self.down1 = Down(64, 128)
+        self.down2 = Down(128, 256)
+        self.down3 = Down(256, 512)
+        self.down4 = Down(512, 1024)  # Bottleneck
+
+        # Decoder (Up sampling)
+        self.up1 = Up(1024, 512)
+        self.up2 = Up(512, 256)
+        self.up3 = Up(256, 128)
+        self.up4 = Up(128, 64)
+
+        # output layer
+        self.outc = nn.Conv2d(64, n_classes, kernel_size=1)
+
+    def forward(self, x):
+        # Encoder path
+        x1 = self.inc(x)
+        x2 = self.down1(x1)
+        x3 = self.down2(x2)
+        x4 = self.down3(x3)
+        x5 = self.down4(x4)
+
+        # Decoder path with Skip Connections
+        x = self.up1(x5, x4)
+        x = self.up2(x, x3)
+        x = self.up3(x, x2)
+        x = self.up4(x, x1)
+
+        logits = self.outc(x)
+        return logits
+
+
+##############################################
+
 
 if __name__ == "__main__":
     # case configuration
@@ -160,7 +258,9 @@ if __name__ == "__main__":
                     current_tensors = []
                     for param in atm_params:
                         file_name = param_maps[param][combined_key]
-                        file_path = os.path.join(wrf_path, param, "proccesed", space_res, time_res, file_name)
+                        file_path = os.path.join(
+                            wrf_path, param, "proccesed", space_res, time_res, file_name
+                        )
                         t = load_nc_layer(file_path, param)
                         if t is not None:
                             current_tensors.append(t)
